9d6c702f4a8b13c61c99f0673e0f44b7665874d2
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-27 21:26:50 +0530

    Add: add new contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,28 @@
-console.log('welcome To The addressBook');
+var prompt = require('prompt-sync')();
+class Contact {
+    
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    }
+    toString() {
+        return "First name: " + this.firstName + "\nLast name: " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city + "\nState: " + this.state + "\nZip: " + this.zip + "\nPhone number: " + this.phoneNumber + "\nEmail: " + this.email;
+    }
+}
+let contact = new Contact("Vaibhav", "Jagtap", "Hadapsar", "Pune", "Maharashtra", 412307, 8007815003, "vaibhav.jagtap03@gmail.com");
+console.log(contact.toString());
